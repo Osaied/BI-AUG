03418e53e839de6b822a5fe5b2e21f745ec4f9db
rahman <osaiedrahaman666@gmail.com>
2021-09-28 23:16:58 +0530

    Assigment.1.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-hjkhkhkh
-jkkhkh
-kjhkjhk
+// var sum = 0; int = 1;
+// while (i <= 10) {
+//     if (i % 2 == 0) {
+//         sum += i;
+
+//     }
+//     i++;
+// }
+// document.write(sum);
+// var str1 = "Hello";
+// var length1 = str1.length; // This returns 5
+
+// var length2 = " Camper".length;
+// var lastNameLength = 0;
+// var lastName = "Lovelace";
+
+// lastNameLength = lastName.length;
+// var text = ""testing: 1,2, 3"";
+// var pattern = /\d+/g; " 
