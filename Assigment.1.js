604a94e010d120954a88b0fb3d848afb1808b9f5
rahman <osaiedrahaman666@gmail.com>
2021-09-28 23:26:27 +0530

    Assigment.1.js

--- Assigment.1.js
+++ Assigment.1.js
@@ -5,76 +5,76 @@
 /* This is a multi-line comment .*/
 
 // 2. Declare javascript  variables:
-// var myName = "Mohammed Osaied";
-// console.log(myName);
+var myName = "Mohammed Osaied";
+console.log(myName);
 
 // 3.String values with the Assingment operator:
-// var a;
-// a = 99;
-// console.log(a);
+var a;
+a = 99;
+console.log(a);
 
 // 4. Assigning the value of one variable to Another:
-// var a;
-// a = 7;
-// var c;
-// c = a;
-// console.log(a);
+var a;
+a = 7;
+var c;
+c = a;
+console.log(a);
 
 // 5.Initializing variables with the Assignment operator:
-// var A = 789;
-// console.log(A);
+var A = 789;
+console.log(A);
 
 // 6.Understanding uninitialied variables:
-// var a = 5;
-// var b = 10;
-// var c = "I am a";
-// console.log(a, b, c);
+var a = 5;
+var b = 10;
+var c = "I am a";
+console.log(a, b, c);
 
 // 7.Understanding Case Sensitivity in Variables:
-// var someVariable;
-// var anotherVariableName;
-// var thisVariableNameIsSoLong;
-// console.log(someVariable);
+var someVariable;
+var anotherVariableName;
+var thisVariableNameIsSoLong;
+console.log(someVariable);
 
 // 8.Add Two Numbers with JavaScript:
-// var myVar = 5 + 10;
-// console.log(myVar);
+var myVar = 5 + 10;
+console.log(myVar);
 
 // 9: Subtract One Number from Another with JavaScript:
-// var myVar = 12 - 6;
-// console.log(myVar);
+var myVar = 12 - 6;
+console.log(myVar);
 
 // 10. Multiply Two Numbers with JavaScript:
-// var myVar = 13 * 13;
-// console.log(myVar);
+var myVar = 13 * 13;
+console.log(myVar);
 
 // 11.Divide One Number by Another with JavaScript:
-// var myVar = 16 / 2;
-// console.log(myVar);
+var myVar = 16 / 2;
+console.log(myVar);
 
 // 12. Increment a Number with JavaScript:
-// i++;
-//  is the equivalent of
-// i = i + 1;
-//  we can write i++ also  is this case .
+i++;
+ is the equivalent of
+i = i + 1;
+ we can write i++ also  is this case .
 
 // 13.  Decrement a Number with JavaScript:
-// i--;
-//  is the equivalent of
-// i = i - 1;
-//  we can write i-- also  is this case .
+i--;
+ is the equivalent of
+i = i - 1;
+ we can write i-- also  is this case .
 
 // 14. Create Decimal Numbers with JavaScript:
-// var myDecimal = 5.7;
-// console.log(myDecimal);
+var myDecimal = 5.7;
+console.log(myDecimal);
 
 // 15. Multiply Two Decimals with JavaScript:
-// var myDecimal = 2.5 * 2.0;
-// console.log(myDecimal);
+var myDecimal = 2.5 * 2.0;
+console.log(myDecimal);
 
 // 16.  Divide One Decimal by Another with JavaScript:
-// var myDecimal = 4.4 / 2;
-// console.log(myDecimal);
+var myDecimal = 4.4 / 2;
+console.log(myDecimal);
 
 // 17. Finding a Remainder in JavaScript:
 /* 11%3 = 2;
@@ -83,123 +83,123 @@ Math.floor(11 / 3) = 3(Quotient)
 11 - 9 = 2(Remainder) */
 
 // 18. Compound Assignment With Augmented Addition:
-// var a = 5;
-// var c = "I am ";
-// a += 1;
-// c += "Rahman";
-// console.log(a, c);
+var a = 5;
+var c = "I am ";
+a += 1;
+c += "Rahman";
+console.log(a, c);
 
 // 19. Compound Assignment With Augmented Subtraction:
-// var a = 5;
-// var b = 4;
-// a -= 1;
-// b -= 3;
-// console.log(a, b,);
+var a = 5;
+var b = 4;
+a -= 1;
+b -= 3;
+console.log(a, b,);
 
 // 20.  Compound Assignment With Augmented Multiplication:
-// var a = a * 5;
-// a *= 9;
-// console.log(a);
+var a = a * 5;
+a *= 9;
+console.log(a);
 
 // 21. Compound Assignment With Augmented Division:
-// a /= 1;
-// b /= 3;
-// E /= "Rahman";
+a /= 1;
+b /= 3;
+E /= "Rahman";
 
 // 22. Declare String Variables:
-// var myFirstName = "Mohammed";
-// var myLastName = "Osaied";
-// console.log(myFirstName, myLastName);
+var myFirstName = "Mohammed";
+var myLastName = "Osaied";
+console.log(myFirstName, myLastName);
 
 // 23.Escaping Literal Quotes in Strings:
-// var myStr = "I am a \"double quoted\"string inside\"double quotes\ "
+var myStr = "I am a \"double quoted\"string inside\"double quotes\ "
 
-// console.log(myStr);
+console.log(myStr);
 
 // 24. Quoting Strings with Single Quotes:
-// goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
-// badStr = 'Finn exclaims to Jake,"Algebraic!"';
-// console.log(goodStr, badStr);
+goodStr = 'Jake asks Finn, "Hey, let\'s go on an adventure?"';
+badStr = 'Finn exclaims to Jake,"Algebraic!"';
+console.log(goodStr, badStr);
 
 // 25. Escape Sequences in Strings:
-// var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
-// console.log(myStr);
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
 
 // 26. Concatenating Strings with Plus Operator:
-// var myStr = "This is the start." + "and This is the end."
-// console.log(myStr);
+var myStr = "This is the start." + "and This is the end."
+console.log(myStr);
 
 // 27.. Concatenating Strings with the Plus Equals Operator:
-// var myStr = "This is the first sentence.";
-// myStr += "and This is the second sentence.";
-// console.log(myStr);
+var myStr = "This is the first sentence.";
+myStr += "and This is the second sentence.";
+console.log(myStr);
 
 // 28. Constructing Strings with Variables:
-// var myName = "Osaied";
-// var myStr = "My name is " + myName + " and I am well";
-// console.log(myStr);
+var myName = "Osaied";
+var myStr = "My name is " + myName + " and I am well";
+console.log(myStr);
 
 // 29.Appending Variables to Strings:
-// var someAdjective = "Board Infinity is";
-// var myStr = "awesome!!";
-// someAdjective += myStr;
-// console.log(someAdjective);
+var someAdjective = "Board Infinity is";
+var myStr = "awesome!!";
+someAdjective += myStr;
+console.log(someAdjective);
 
 // 30. Use Bracket Notation to Find the First Character in a String:
-// var firstName = "Mohammed";
-// var LastName = "Osaied";
-// var firstNameOfLastName = LastName[0];
+var firstName = "Mohammed";
+var LastName = "Osaied";
+var firstNameOfLastName = LastName[0];
 
 // 31. Find the Length of a String:
-// var firstName = "Osaied";
-// var LastName = "Rahman";
-// console.log(LastName.length);
+var firstName = "Osaied";
+var LastName = "Rahman";
+console.log(LastName.length);
 
 // 32. Understand String Immutability:
-// var myStr = "job";
-// myStr = "Hello world";
-// console.log(myStr);
+var myStr = "job";
+myStr = "Hello world";
+console.log(myStr);
 
 // 33.Use Bracket Notation to Find the Nth Character in a String:
-// var firstName = "Ada";
-// var thirdLetterOfLastName = firstName[2];
+var firstName = "Ada";
+var thirdLetterOfLastName = firstName[2];
 
 // 34.Use Bracket Notation to Find the Last Character in a String:
-// var LastName = "RAHMAN";
-// var lastCharacter = LastName[LastName.length - 1];
-// console.log(lastCharacter);
+var LastName = "RAHMAN";
+var lastCharacter = LastName[LastName.length - 1];
+console.log(lastCharacter);
 
 // 35.Use Bracket Notation to Find the Nth-to-Last Character in a String:
-// var LastName = "OSAIED";
-// var secondToLastLetter = LastName[LastName.length - 3];
-// console.log(secondToLastLetter);
+var LastName = "OSAIED";
+var secondToLastLetter = LastName[LastName.length - 3];
+console.log(secondToLastLetter);
 
 // 36.Word Blanks:
-// var myNoun = "dog";
-// var myAdjective = "big";
-// var myVerb = "ran";
-// var myAdverb = "quickly";
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
 
-// var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " " + ".";
-// console.log(wordBlanks);
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " " + ".";
+console.log(wordBlanks);
 
 // 37. Store Multiple Values in one Variable using JavaScript Arrays:
-// var myArray = ["peanut butter", "jelly", 1234, "bread"];;
-// console.log(myArray);
+var myArray = ["peanut butter", "jelly", 1234, "bread"];;
+console.log(myArray);
 
 // 38. Nest one Array within Another Array:
-// var myArray = [["Tiger", 23], ["Lion", 45], ["Cheeta", 50]];
-// console.log(myArray);
+var myArray = [["Tiger", 23], ["Lion", 45], ["Cheeta", 50]];
+console.log(myArray);
 
 // 39. Access Array Data with Indexes:
-// var myArray = [50, 100, 780];
-// var myData = myArray[0];
-// console.log(myData);
+var myArray = [50, 100, 780];
+var myData = myArray[0];
+console.log(myData);
 
 // 40.Modify Array Data With Indexes:
-// var myArray = [550, 560, 460];
-// myArray[0] = 45;
-// console.log(myArray[0]);
+var myArray = [550, 560, 460];
+myArray[0] = 45;
+console.log(myArray[0]);
 
 
 
